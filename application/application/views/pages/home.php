f7a205bc9a4a7c61c5abb4aeee4d57fec95fa9fe
danijela-vaske <daca.nesavaskovic@hotmail.com>
2019-04-09 23:53:24 +0200

    Upload new files

---
+++ application/application/views/pages/home.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p><strong>Welcome to the CiBlog application!</strong></p>
---
+++ application/application/views/posts/view.php
@@ -0,0 +1,17 @@
+<h2><?php echo $post['title']; ?></h2>
+<small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br>
+<img src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>">
+<div class="post-body">
+<?php echo $post['body']; ?>
+</div>
+
+<hr>
+<div class="btn-group">
+
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+	<input type="submit" value="Delete" class="btn btn-danger">
+
+	<a class="btn btn-primary" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
+
+</form>
+</div>
